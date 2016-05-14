74e8fcf98fbac1797a05a3f0f5679bfdf1d4ea3a
Connor Bray <connor.bray@icloud.com>
2016-05-14 16:52:22 +0100

    Delete junk files, this package doesnt require a UI right now.

--- spec/atom-maven-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomMavenView from '../lib/atom-maven-view';
-
-describe('AtomMavenView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
