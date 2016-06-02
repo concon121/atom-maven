2263a2e8c841d812f27fd5669837f03f97219097
Connor Bray <connor.bray@icloud.com>
2016-06-02 23:39:16 +0100

    Tidy up tests

--- spec/maven-utils-spec.js
+++ spec/maven-utils-spec.js
@@ -5,85 +5,87 @@ const ps = common.pathSeparator;
 
 describe('When finding the location of the maven settings file.', () => {
 
+	var pathPrefix = (common.isWin) ? 'C:' + fs : fs;
+
 	beforeEach(function () {
 		spyOn(common, 'resolveEnvironmentVariable').andReturn('C:' + fs + 'tools' + fs + 'apache-maven');
 	});
 
 	it('should correctly resolve the maven settings.xml location when the path contains only the maven home url.', () => {
-		var path = 'C:' + fs + 'tools' + fs + 'apache-maven' + fs + 'bin';
+		var path = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'bin';
 		var actualSettings = mvn.getMavenGlobalSettings(path);
-		var expectedSettings = 'C:' + fs + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
+		var expectedSettings = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
 	it('should correctly resolve the maven settings.xml location when the path contains more than 1 entry.', () => {
-		var path = 'C:' + fs + 'tools' + fs + 'random' + fs + 'bin' + ps +
+		var path = pathPrefix + 'tools' + fs + 'random' + fs + 'bin' + ps +
 			'C:' + fs + 'tools' + fs + 'apache-maven' + fs + 'bin' + ps +
 			'C:' + fs + 'tools' + fs + 'other-random' + fs + 'bin' + ps;
 		var actualSettings = mvn.getMavenGlobalSettings(path);
-		var expectedSettings = 'C:' + fs + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
+		var expectedSettings = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
 	it('should in the case that there are multiple maven entries on the PATH, prefer to use the first entry defined.', () => {
-		var path = 'C:' + fs + 'tools' + fs + 'random' + fs + 'bin' + ps +
-			'C:' + fs + 'tools' + fs + 'apache-maven-correct' + fs + 'bin' + ps +
-			'C:' + fs + 'tools' + fs + 'other-random' + fs + 'bin' + ps +
-			'C:' + fs + 'tools' + fs + 'apache-maven-incorrect' + fs + 'bin' + ps;
+		var path = pathPrefix + 'tools' + fs + 'random' + fs + 'bin' + ps +
+			pathPrefix + 'tools' + fs + 'apache-maven-correct' + fs + 'bin' + ps +
+			pathPrefix + 'tools' + fs + 'other-random' + fs + 'bin' + ps +
+			pathPrefix + 'tools' + fs + 'apache-maven-incorrect' + fs + 'bin' + ps;
 		var actualSettings = mvn.getMavenGlobalSettings(path);
-		var expectedSettings = 'C:' + fs + 'tools' + fs + 'apache-maven-correct' + fs + 'conf' + fs + 'settings.xml';
+		var expectedSettings = pathPrefix + 'tools' + fs + 'apache-maven-correct' + fs + 'conf' + fs + 'settings.xml';
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
 	it('should correctly resolve the maven settings.xml location when the maven home directory is an environment variable.', () => {
-		var path = 'M2_HOME' + fs + 'bin';
+		var path = pathPrefix + 'M2_HOME' + fs + 'bin';
 		var actualSettings = mvn.getMavenGlobalSettings(path);
-		var expectedSettings = 'C:' + fs + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
+		var expectedSettings = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
 	it('should correctly resolve the maven settings.xml location where there is more than one entry on the PATH and the maven home directory is an environment variable.', () => {
-		var path = 'C:' + fs + 'tools' + fs + 'random' + fs + 'bin' + ps +
-			'M2_HOME' + fs + 'bin' + ps +
-			'C:' + fs + 'tools' + fs + 'other-random' + fs + 'bin' + ps;
+		var path = pathPrefix + 'tools' + fs + 'random' + fs + 'bin' + ps +
+			pathPrefix + 'M2_HOME' + fs + 'bin' + ps +
+			pathPrefix + 'tools' + fs + 'other-random' + fs + 'bin' + ps;
 		var actualSettings = mvn.getMavenGlobalSettings(path);
-		var expectedSettings = 'C:' + fs + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
+		var expectedSettings = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
 	it('should in the case that there are multiple maven entries on the PATH, prefer to use the first entry defined, when the maven home directories are environment variables.', () => {
-		var path = 'C:' + fs + 'tools' + fs + 'random' + fs + 'bin' + ps +
-			'M2_HOME' + fs + 'bin' + ps +
-			'C:' + fs + 'tools' + fs + 'other-random' + fs + 'bin' + ps +
-			'M2_HOME' + fs + 'bin' + ps;
+		var path = pathPrefix + 'tools' + fs + 'random' + fs + 'bin' + ps +
+			pathPrefix + 'M2_HOME' + fs + 'bin' + ps +
+			pathPrefix + 'tools' + fs + 'other-random' + fs + 'bin' + ps +
+			pathPrefix + 'M2_HOME' + fs + 'bin' + ps;
 		var actualSettings = mvn.getMavenGlobalSettings(path);
-		var expectedSettings = 'C:' + fs + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
+		var expectedSettings = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
 	it('should resolve the maven settings location if the first maven entry on the classpath does not contain maven binaries', () => {
-		var path = 'M2';
+		var path = pathPrefix + 'M2';
 		var actualSettings = mvn.getMavenGlobalSettings(path);
-		var expectedSettings = 'C:' + fs + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
+		var expectedSettings = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
 	it('should resolve the maven settings location if the first maven entry on the classpath does not contain maven binaries and there is more than 1 entry on the PATH.', () => {
-		var path = 'C:' + fs + 'tools' + fs + 'random' + fs + 'bin' + ps +
-			'M2' + ps +
-			'C:' + fs + 'tools' + fs + 'other-random' + fs + 'bin' + ps;
+		var path = pathPrefix + 'tools' + fs + 'random' + fs + 'bin' + ps +
+			pathPrefix + 'M2' + ps +
+			pathPrefix + 'tools' + fs + 'other-random' + fs + 'bin' + ps;
 		var actualSettings = mvn.getMavenGlobalSettings(path);
-		var expectedSettings = 'C:' + fs + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
+		var expectedSettings = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
 	it('should resolve the maven settings location if the first maven entry on the classpath does not contain maven binaries and there are multiple maven entries on the PATH.', () => {
-		var path = 'C:' + fs + 'tools' + fs + 'random' + fs + 'bin' + ps +
-			'M2' + ps +
-			'C:' + fs + 'tools' + fs + 'other-random' + fs + 'bin' + ps +
-			'M2' + ps;
+		var path = pathPrefix + 'tools' + fs + 'random' + fs + 'bin' + ps +
+			pathPrefix + 'M2' + ps +
+			pathPrefix + 'tools' + fs + 'other-random' + fs + 'bin' + ps +
+			pathPrefix + 'M2' + ps;
 		var actualSettings = mvn.getMavenGlobalSettings(path);
-		var expectedSettings = 'C:' + fs + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
+		var expectedSettings = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 });
@@ -113,4 +115,5 @@ describe('When maven-utils detects that maven has been found and installed corre
 		mvn.getMavenGlobalSettings(path);
 		expect(mvn.mavenIsInstalled).toEqual(true);
 	});
+
 });
