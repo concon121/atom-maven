c5b0f6236ed68d5cdcb1607d29284a9a31450176
Connor Bray <connor.bray@icloud.com>
2016-06-04 01:02:38 +0100

    Update spec, istanbul moaning on travis

--- spec/maven-utils-spec.js
+++ spec/maven-utils-spec.js
@@ -8,21 +8,21 @@ const fs = common.fileSeparator;
 const ps = common.pathSeparator;
 const pathPrefix = (common.isWin) ? 'C:' + fs : fs;
 
-describe('When finding the location of the maven settings file.', () => {
+describe('When finding the location of the maven settings file.', function () {
 
 	beforeEach(function () {
 		spyOn(common, 'resolveEnvironmentVariable').andReturn(pathPrefix + 'tools' + fs + 'apache-maven');
 		spyOn(ui, 'warning');
 	});
 
-	it('should correctly resolve the maven settings.xml location when the path contains only the maven home url.', () => {
+	it('should correctly resolve the maven settings.xml location when the path contains only the maven home url.', function () {
 		var path = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'bin';
 		var actualSettings = mvn.getMavenGlobalSettings(path);
 		var expectedSettings = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
-	it('should correctly resolve the maven settings.xml location when the path contains more than 1 entry.', () => {
+	it('should correctly resolve the maven settings.xml location when the path contains more than 1 entry.', function () {
 		var path = pathPrefix + 'tools' + fs + 'random' + fs + 'bin' + ps +
 			pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'bin' + ps +
 			pathPrefix + 'tools' + fs + 'other-random' + fs + 'bin' + ps;
@@ -31,7 +31,7 @@ describe('When finding the location of the maven settings file.', () => {
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
-	it('should in the case that there are multiple maven entries on the PATH, prefer to use the first entry defined.', () => {
+	it('should in the case that there are multiple maven entries on the PATH, prefer to use the first entry defined.', function () {
 		var path = pathPrefix + 'tools' + fs + 'random' + fs + 'bin' + ps +
 			pathPrefix + 'tools' + fs + 'apache-maven-correct' + fs + 'bin' + ps +
 			pathPrefix + 'tools' + fs + 'other-random' + fs + 'bin' + ps +
@@ -41,14 +41,14 @@ describe('When finding the location of the maven settings file.', () => {
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
-	it('should correctly resolve the maven settings.xml location when the maven home directory is an environment variable.', () => {
+	it('should correctly resolve the maven settings.xml location when the maven home directory is an environment variable.', function () {
 		var path = pathPrefix + 'M2_HOME' + fs + 'bin';
 		var actualSettings = mvn.getMavenGlobalSettings(path);
 		var expectedSettings = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
-	it('should correctly resolve the maven settings.xml location where there is more than one entry on the PATH and the maven home directory is an environment variable.', () => {
+	it('should correctly resolve the maven settings.xml location where there is more than one entry on the PATH and the maven home directory is an environment variable.', function () {
 		var path = pathPrefix + 'tools' + fs + 'random' + fs + 'bin' + ps +
 			pathPrefix + 'M2_HOME' + fs + 'bin' + ps +
 			pathPrefix + 'tools' + fs + 'other-random' + fs + 'bin' + ps;
@@ -57,7 +57,7 @@ describe('When finding the location of the maven settings file.', () => {
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
-	it('should in the case that there are multiple maven entries on the PATH, prefer to use the first entry defined, when the maven home directories are environment variables.', () => {
+	it('should in the case that there are multiple maven entries on the PATH, prefer to use the first entry defined, when the maven home directories are environment variables.', function () {
 		var path = pathPrefix + 'tools' + fs + 'random' + fs + 'bin' + ps +
 			pathPrefix + 'M2_HOME' + fs + 'bin' + ps +
 			pathPrefix + 'tools' + fs + 'other-random' + fs + 'bin' + ps +
@@ -67,14 +67,14 @@ describe('When finding the location of the maven settings file.', () => {
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
-	it('should resolve the maven settings location if the first maven entry on the classpath does not contain maven binaries', () => {
+	it('should resolve the maven settings location if the first maven entry on the classpath does not contain maven binaries', function () {
 		var path = pathPrefix + 'M2';
 		var actualSettings = mvn.getMavenGlobalSettings(path);
 		var expectedSettings = pathPrefix + 'tools' + fs + 'apache-maven' + fs + 'conf' + fs + 'settings.xml';
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
-	it('should resolve the maven settings location if the first maven entry on the classpath does not contain maven binaries and there is more than 1 entry on the PATH.', () => {
+	it('should resolve the maven settings location if the first maven entry on the classpath does not contain maven binaries and there is more than 1 entry on the PATH.', function () {
 		var path = pathPrefix + 'tools' + fs + 'random' + fs + 'bin' + ps +
 			pathPrefix + 'M2' + ps +
 			pathPrefix + 'tools' + fs + 'other-random' + fs + 'bin' + ps;
@@ -83,7 +83,7 @@ describe('When finding the location of the maven settings file.', () => {
 		expect(actualSettings).toEqual(expectedSettings);
 	});
 
-	it('should resolve the maven settings location if the first maven entry on the classpath does not contain maven binaries and there are multiple maven entries on the PATH.', () => {
+	it('should resolve the maven settings location if the first maven entry on the classpath does not contain maven binaries and there are multiple maven entries on the PATH.', function () {
 		var path = pathPrefix + 'tools' + fs + 'random' + fs + 'bin' + ps +
 			pathPrefix + 'M2' + ps +
 			pathPrefix + 'tools' + fs + 'other-random' + fs + 'bin' + ps +
@@ -94,14 +94,14 @@ describe('When finding the location of the maven settings file.', () => {
 	});
 });
 
-describe('When maven-utils detects that maven has not been installed correctly.', () => {
+describe('When maven-utils detects that maven has not been installed correctly.', function () {
 
 	beforeEach(function () {
 		spyOn(common, 'resolveEnvironmentVariable').andReturn(pathPrefix + 'tools' + fs + 'apache-maven');
 		spyOn(ui, 'warning');
 	});
 
-	it('should set the flag "mavenIsInstalled" to false, to indicate that it should not try to load pom files.', () => {
+	it('should set the flag "mavenIsInstalled" to false, to indicate that it should not try to load pom files.', function () {
 		var path = '';
 		mvn.getMavenGlobalSettings(path);
 		expect(mvn.mavenIsInstalled).toEqual(false);
@@ -109,13 +109,13 @@ describe('When maven-utils detects that maven has not been installed correctly.'
 });
 
 
-describe('When maven-utils detects that maven has been found and installed correctly.', () => {
+describe('When maven-utils detects that maven has been found and installed correctly.', function () {
 
 	beforeEach(function () {
 		spyOn(common, 'resolveEnvironmentVariable').andReturn(pathPrefix + 'tools' + fs + 'apache-maven');
 	});
 
-	it('should set the flag "mavenIsInstalled" to true, to indicate that it should load the pom files in the workspace.', () => {
+	it('should set the flag "mavenIsInstalled" to true, to indicate that it should load the pom files in the workspace.', function () {
 		var path = pathPrefix + 'M2';
 		mvn.getMavenGlobalSettings(path);
 		expect(mvn.mavenIsInstalled).toEqual(true);
