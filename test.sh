52afa396d92e42ae2aa6ecbe025e48b70dd6ca73
Connor Bray <connor.bray@icloud.com>
2016-06-11 10:54:14 +0100

    Trialing codacy

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ then
     npm install
     npm run coverage
     codeclimate-test-reporter < coverage/lcov.info
+    codacy-coverage < coverage/lcov.info
 else
     jasmine-focused spec
     npm run quality
