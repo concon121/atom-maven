ffa19ee6a33c8d37bcce4acba55a100526b64021
Connor Bray <connor.bray@icloud.com>
2016-06-12 13:00:19 +0100

    Update travis configuration

--- test.sh
+++ test.sh
@@ -2,11 +2,10 @@
 
 if [ $COVERAGE == true ]
 then
-    npm install
     npm run coverage
     codeclimate-test-reporter < coverage/lcov.info
     codacy-coverage < coverage/lcov.info
 else
-    jasmine-focused spec
+    npm test
     npm run quality
 fi
